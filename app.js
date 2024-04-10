13e9e35d5ed68bce861402ee075ba29ced1704c9
jjikky <kok5417@skuniv.ac.kr>
2024-04-10 14:06:06 +0900

    🎨 express 적용

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(morgan('dev'));
+app.use(helmet());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+app.get('/user/update', (req, res) => {
+    res.render('user-update');
+});
+app.get('/board', (req, res) => {
+    // TODO : 더미 넣기
+    res.render('board');
+});
+app.get('/board/:id', (req, res) => {
+    // TODO : ID에 맞는 데이터 전달
+    const id = req.params.id;
+    res.render('board-detail');
+});
+
+app.get('/board/create', (req, res) => {
+    res.render('board-create');
+});
+app.get('/board/update', (req, res) => {
+    res.render('board-update');
+});
+
+app.all('*', (req, res) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server`,
+    });
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`app listening on http://localhost:${port}`);
+});
