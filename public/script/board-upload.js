ef7c0ba168e8dc9898ca67914c85e2276bd5c69d
jjikky <kok5417@skuniv.ac.kr>
2024-04-17 11:46:33 +0900

    ✨ 게시글 작성 페이지 : 완료 버튼 활성화 로직 작성

---
+++ public/script/board-upload.js
@@ -0,0 +1,35 @@
+userNav = document.getElementById('user-nav');
+profileBtn = document.getElementById('profile-btn');
+profileBtn.addEventListener('click', () => {
+    if (userNav.style.display == 'flex') {
+        userNav.style.display = 'none';
+    } else {
+        userNav.style.display = 'flex';
+    }
+});
+
+const uploadBtn = document.getElementById('upload-btn');
+const uploadForm = document.getElementById('upload-form');
+
+// 제목 내용 입력에 따른 버튼 활성화
+uploadForm.title.addEventListener('input', (e) => {
+    if (e.target.value.length > 0 && uploadForm.content.value.length > 0)
+        return (uploadBtn.style.backgroundColor = '#7F6AEE');
+    uploadBtn.style.backgroundColor = '#ACA0EB';
+});
+uploadForm.content.addEventListener('input', (e) => {
+    if (e.target.value.length > 0 && uploadForm.title.value.length > 0)
+        return (uploadBtn.style.backgroundColor = '#7F6AEE');
+    uploadBtn.style.backgroundColor = '#ACA0EB';
+});
+
+uploadBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (uploadForm.title.value.length > 0 && uploadForm.content.value.length) {
+        alert('작성 완료!');
+        location.href = 'http://localhost:3000/board';
+    } else {
+        const helperText = document.getElementsByClassName('helper-text');
+        helperText[0].innerHTML = '*제목, 내용을 모두 작성해주세요';
+    }
+});
