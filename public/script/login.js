552f6f958a4cd499c215c205530d808e83f38b55
jjikky <kok5417@skuniv.ac.kr>
2024-04-18 09:44:19 +0900

    ♻️ 로그인 유효성 검증 리팩토링

--- public/script/login.js
+++ public/script/login.js
@@ -1,30 +1,42 @@
 // login page : 실시간 이메일 검증
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const loginHelper = document.getElementById('login-helper');
+const emailHelper = document.getElementById('email-helper');
+const passwordHelper = document.getElementById('password-helper');
 const loginBtn = document.getElementById('login-btn');
 loginBtn.addEventListener('click', clickLoginBtn);
 
 const emailPattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+
+let isComplete = {
+    email: 0,
+    password: 0,
+};
+
 email.addEventListener('input', (event) => {
-    // 유효 x
-    if (validateEmail(event.target.value) && validatePassword(document.getElementById('password').value)) {
-        loginHelper.innerHTML = '';
-        loginBtn.style.backgroundColor = '#7F6AEE';
-    } else {
-        loginHelper.innerHTML = '입력하신 계정 정보가 정확하지 않습니다.';
-        loginBtn.style.backgroundColor = '#ACA0EB';
+    disableButton('login-btn');
+    if (validateEmail(event.target.value)) {
+        emailHelper.innerHTML = '';
+        if (validatePassword(document.getElementById('password').value)) {
+            return activeButton('login-btn');
+        }
+        return;
     }
+    emailHelper.innerHTML = '*올바른 이메일 주소 형식을 입력해주세요. (예:example@example.com)';
 });
 password.addEventListener('input', (event) => {
-    // 유효 x
-    if (validatePassword(event.target.value) && validateEmail(document.getElementById('email').value)) {
-        loginHelper.innerHTML = '';
-        loginBtn.style.backgroundColor = '#7F6AEE';
-    } else {
-        loginHelper.innerHTML = '입력하신 계정 정보가 정확하지 않습니다.';
-        loginBtn.style.backgroundColor = '#ACA0EB';
+    disableButton('login-btn');
+    if (validatePassword(event.target.value)) {
+        // activeButton('login-btn');
+        passwordHelper.innerHTML = '';
+        if (validateEmail(document.getElementById('email').value)) {
+            return activeButton('login-btn');
+        }
+        return;
     }
+    passwordHelper.innerHTML =
+        '*비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수문자를 각각 <br>최소 1개 포함해야합니다.';
 });
 
 // 이메일 유효성 검증
@@ -39,6 +51,7 @@ function validateEmail(email) {
 // 비밀번호 유효성 검증
 function validatePassword(password) {
     if (password === '') return false;
+    if (passwordPattern.test(password) === false) return false;
     return true;
 }
 
