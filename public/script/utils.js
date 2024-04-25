b62f4e6678bf6daefec490ca43aede2167d7d44c
jjikky <kok5417@skuniv.ac.kr>
2024-04-25 13:58:22 +0900

    ♻️ util es6문법으로 변경

--- public/script/utils.js
+++ public/script/utils.js
@@ -3,20 +3,16 @@ const TITLE_MAX_LENGTH = 26;
 
 // home navigation
 const home = document.getElementById('header-text');
-home.addEventListener('click', () => {
-    window.location.href = 'http://localhost:3000/post';
-});
+home.addEventListener('click', () => (window.location.href = 'http://localhost:3000/post'));
 
 // 뒤로가기
-function backIconClick(el) {
-    el.addEventListener('click', () => {
-        history.back();
-    });
+const backIconClick = (el) => {
+    el.addEventListener('click', () => history.back());
     el.style.cursor = 'pointer';
-}
+};
 
 // 조회수,댓글,좋아요 수 등 형식 변환
-function formatCount(count) {
+const formatCount = (count) => {
     if (count >= 1000000) {
         return (count / 1000000).toFixed(1) + 'm';
     } else if (count >= 1000) {
@@ -24,35 +20,35 @@ function formatCount(count) {
     } else {
         return count.toString();
     }
-}
+};
 
 // 모달
-function openModal(modalName, overlayName) {
+const openModal = (modalName, overlayName) => {
     const modal = document.querySelector(modalName);
     modal.style.display = 'flex';
     document.querySelector(overlayName).style.display = 'flex';
     document.querySelector('body').style.overflow = 'hidden';
-}
+};
 
-function closeModal(modalName, overlayName) {
+const closeModal = (modalName, overlayName) => {
     const modal = document.querySelector(modalName);
     modal.style.display = 'none';
     document.querySelector(overlayName).style.display = 'none';
     document.querySelector('body').style.overflow = 'auto';
-}
+};
 
 // 버튼 활성화, 비활성화
-function activeButton(id) {
+const activeButton = (id) => {
     let button = document.getElementById(id);
     button.classList.add('active');
-}
-function disableButton(className) {
+};
+const disableButton = (className) => {
     let button = document.getElementById(className);
     button.classList.remove('active');
-}
+};
 
 // 입력 필드가 모두 채워졌는지 확인하는 함수
-function checkInputs(inputs, buttonId) {
+const checkInputs = (inputs, buttonId) => {
     let isValid = true;
 
     inputs.forEach((v) => {
@@ -67,20 +63,20 @@ function checkInputs(inputs, buttonId) {
     } else {
         disableButton(buttonId);
     }
-}
+};
 
-function logout() {
+const logout = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('user_id');
     window.location.href = 'http://localhost:3000/login';
-}
+};
 
-function insertHeaderAvatar(user_avatar) {
+const insertHeaderAvatar = (user_avatar) => {
     const profileBtn = document.getElementById('profile-btn');
     profileBtn.setAttribute('src', user_avatar);
-}
+};
 
-function insertFormAvatar(user_avatar) {
+const insertFormAvatar = (user_avatar) => {
     const avatarImg = document.getElementById('avatar');
     avatarImg.setAttribute('src', user_avatar);
-}
+};
